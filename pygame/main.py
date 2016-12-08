e52bfe653ce865e6afe5068fd8ef4ee9a5e9fed6
louisalflame <louisalflame@hotmail.com.tw>
2016-12-08 23:11:35 +0800

    window -> scene

--- pygame/main.py
+++ pygame/main.py
@@ -15,30 +15,50 @@ title = "Hello, Pygame!"
 chinese_message = "來寫遊戲吧！"
 message = str(chinese_message)
  
+
+
+
+class WindowManager:
+    def __init__(self, size):
+        pygame.init()
+
+        screen = pygame.display.set_mode(size, 0, 32)
+
+    def setTitle(self, title):
+        pygame.display.set_caption(title)
+
+    def setIcon(self, iconFile):
+        icon = pygame.image.load(iconFile)
+        icon = pygame.transform.scale( icon, (32,32) )
+        pygame.display.set_icon(icon)
+
+    def draw(self):
+        font = util.font_MsjhBd(80)
+        text = font.render(message, True, white)
+     
+        x = (size[0]-text.get_width()) / 2
+        y = (size[1]-text.get_height()) / 2
+
+        screen = pygame.display.get_surface()
+        screen.fill(black)
+        screen.blit(text, (x, y))
+
+    def update(self):
+        pygame.display.update()
+
+
 def run():
-    pygame.init()
- 
-    screen = pygame.display.set_mode(size, 0, 32)
-    pygame.display.set_caption(title)
-    icon = pygame.image.load(r"C:\Users\user\Desktop\DiceGame\panel\AttrAtk.png")
-    icon = pygame.transform.scale( icon, (32,32) )
-    pygame.display.set_icon(icon)
- 
-    font = util.fontMsjhBd(80)
-    text = font.render(message, True, white)
- 
-    x = (size[0]-text.get_width()) / 2
-    y = (size[1]-text.get_height()) / 2
+    window = WindowManager( (800,600) )
+    window.setTitle( "來寫遊戲吧！" )
+    window.setIcon(r"C:\Users\user\Desktop\DiceGame\panel\AttrAtk.png") 
  
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 exit()
  
-        screen.fill(black)
-        screen.blit(text, (x, y))
- 
-        pygame.display.update()
+        window.draw()
+        window.update()
  
 if __name__ == "__main__":
     run()
--- pygame/util.py
+++ pygame/util.py
@@ -4,5 +4,5 @@ import pygame
 from pygame.locals import *
 import os
 
-def fontMsjhBd(size):
+def font_MsjhBd(size):
 	return pygame.font.Font(os.environ['SYSTEMROOT'] + "\\Fonts\\msjhbd.ttf", size)
